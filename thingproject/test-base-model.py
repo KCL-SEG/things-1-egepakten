4061b9d0991843e9e4c8962e5d0d28d46ee4ec71
Pakten, Kerem (k20057059) <kerem_ege.pakten@kcl.ac.uk>
2022-12-08 23:07:47 +0000

    Title fixed

--- thingproject/test-base-model.py
+++ thingproject/test-base-model.py
@@ -3,7 +3,7 @@
 
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from things.models import Thing
+from thingproject.models import Thing
 
 class BaseModelTest(TestCase):
     def setUp(self):
