b73790a93eb1f25b8f68476d2f6fdbb6c1e918f7
Pakten, Kerem (k20057059) <kerem_ege.pakten@kcl.ac.uk>
2022-12-08 22:58:01 +0000

    Second try

--- thingproject/models.py
+++ thingproject/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
-
     name = models.CharField(blank=False, unique=True,max_length = 120)
     description = models.CharField(blank=True, unique=False,max_length = 120)
     quantity =  models.IntegerField(default=0,
